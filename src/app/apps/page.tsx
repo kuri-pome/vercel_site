5d0896c0eaca0e940e48eae1cacaebd8ed8f0963
kuri_pome <kuri_pome>
2024-02-11 03:24:43 +0000

    feat: ちょっとした修正apps

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import Link from "next/link"
 
-export const page = () => {
+const page = () => {
 
   const apps: Array<string> = [
     "ev",
   ];
   return (
     <>
-      <h1>app一覧</h1>
-      <ul>
-        {
-          apps.map((apps, index) => (
-            <li key={index}><Link href={`/apps/${apps}`}>{apps}</Link></li>
-          ))
-        }
-      </ul>
+      <div className='justify-center items-center'>
+        <div className='text-3xl'>
+          <h1>app一覧</h1>
+        </div>
+        <div className="bg-green-400">
+          <ul>
+            {
+              apps.map((apps, index) => (
+                <li key={index}><Link href={`/apps/${apps}`}>・{apps}</Link></li>
+              ))
+            }
+          </ul>
+        </div>
+      </div >
     </>
   )
 };

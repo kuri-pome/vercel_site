f4663042f058f78c8dd3045471ca0cbe3acd1562
kuri_pome <kuri_pome>
2024-05-24 14:54:20 +0000

    refactor: api version. title

--- src/app/components/layouts/footer/Footer.tsx
+++ src/app/components/layouts/footer/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer>
       <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
-        <h3>Designed by Kuripome</h3>
+        <h1>Designed by Kuripome. All rights reserved.</h1>
       </div>
     </footer>
   )
--- src/app/components/layouts/header/Header.tsx
+++ src/app/components/layouts/header/Header.tsx
@@ -3,26 +3,41 @@ import styles from "./Header.module.css"
 
 
 export const Header = () => {
-  const headers: Array<string> = [
-    "profile",
-    "work",
-    "blog",
-    "apps",
+  const menuItems: Array<string> = [
+    { name: "profile", link: "/profile" },
+    { name: "work", link: "/work" },
+    { name: "blog", link: "/blog" },
+    { name: "apps", link: "/apps" },
   ]
 
   return (
-    // <header className={styles.root_title}>
-    <header>
-      <nav className={styles.nav}>
-        <h1><Link href="/">Kuripome Site</Link></h1>
-        <ul className={styles.ul}>
-          {
-            headers.map((header, index) => (
-              <li key={index} className={styles.li}><Link href={`/${header}`}>{header}</Link></li>
-            ))
-          }
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className={styles.container}>
+
+        <div className={styles.site_name}>
+              <Link href="/">Kuripome Site</Link>
+        </div>
+        <nav>
+            <ul className={styles.menu_list}>
+                {menuItems.map((menuItem) => (
+                    <li className={styles.menu_item} key={menuItem.name}>
+                        <Link href={menuItem.link} className={styles.menu_link}>{menuItem.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+
+        {/* <nav className={styles.nav}>
+          <h1><Link href="/">Kuripome Site</Link></h1>
+          <ul className={styles.ul}>
+            {
+              headers.map((header, index) => (
+                <li key={index} className={styles.li}><Link href={`/${header}`}>{header}</Link></li>
+              ))
+            }
+          </ul>
+        </nav> */}
+      </header>
+    </>
   )
 }
